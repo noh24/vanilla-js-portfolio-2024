fe0196697058147bc75f2868770b1053c0271bcf
Brian <brian.noh95@gmail.com>
2024-12-29 19:43:58 -0500

    server.js - put all code in new class  StaticFileServer

--- server.js
+++ server.js
@@ -4,93 +4,102 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import livereload from 'livereload'
 
-const __fileName = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__fileName)
-const mimeTypes = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
+const MIME_TYPES = {
+	'.ico': { contentType: 'image/x-icon', directory: '' },
+	'.html': { contentType: 'text/html', directory: 'src' },
+	'.js': { contentType: 'text/javascript', directory: 'src' },
+	'.css': { contentType: 'text/css', directory: 'src' },
+	'.ttf': { contentType: 'font/ttf', directory: '' },
+	'.svg': { contentType: 'image/svg+xml', directory: '' },
 }
 const PORT = 3000
+const IS_DEV = process.env.NODE_ENV.trim() === 'development'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const readFileWithLiveReload = (filePath, contentType, callBack) => {
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            return callBack(err)
-        }
+class StaticFileServer {
+	constructor(port) {
+		this.port = port
+		this.server = http.createServer(this.handleRequest.bind(this))
+	}
 
-        if (contentType === 'text/html' && process.env.NODE_ENV === 'development ') {
-            data = data.toString().replace(
-                '</body>',
-                `<script src="http://localhost:35729/livereload.js"></script></body>`
-            )
-            return callBack(null, data)
-        }
+	async readFile(filePath) {
+		return fs.promises.readFile(filePath)
+	}
 
-        return callBack(null, data)
-    })
+	injectLiveReload(html) {
+		return html.toString().replace(
+			'</body>',
+			`<script src="http://localhost:35729/livereload.js"></script></body>`,
+		)
+	}
+
+	async serveFile(req, res) {
+		const urlPath = req.url === '/' ? '/index.html' : req.url
+		const ext = path.extname(urlPath)
+		const mimeInfo = MIME_TYPES[ext] || { contentType: 'text/plain', directory: '' }
+		const filePath = path.join(__dirname, mimeInfo.directory, urlPath)
+
+		try {
+			let data = await this.readFile(filePath)
+
+			if (mimeInfo.contentType === 'text/html' && IS_DEV) {
+				data = this.injectLiveReload(data)
+			}
+
+			res.writeHead(200, { 'content-type': mimeInfo.contentType })
+			res.end(data)
+		} catch (err) {
+			throw err
+		}
+	}
+
+	async handleError(res) {
+		try {
+			const data = await this.readFile(path.join(__dirname, 'src', 'index.html'))
+			res.writeHead(301, { Location: '/', 'content-type': 'text/html' })
+			res.end(data)
+		} catch (err) {
+			res.writeHead(500, { 'content-type': 'text/plain' })
+			res.end('Internal Server Error')
+		}
+	}
+
+	async handleRequest(req, res) {
+		try {
+			await this.serveFile(req, res)
+		} catch (err) {
+			await this.handleError(res)
+		}
+	}
+
+	setupLiveReload() {
+		if (!IS_DEV) return
+
+		const liveReloadServer = livereload.createServer({
+			exts: ['html', 'js', 'css'],
+		})
+
+		liveReloadServer.watch(path.join(__dirname, 'src'))
+
+		let debounceTimer
+		fs.watch(path.join(__dirname, 'src'), { recursive: true }, (_, fileName) => {
+			if (fileName) {
+				clearTimeout(debounceTimer)
+				debounceTimer = setTimeout(() => {
+					console.log(`File changed: ${fileName}`)
+					liveReloadServer.refresh('*')
+				}, 300)
+			}
+		})
+	}
+
+	start() {
+		this.server.listen(this.port, () => {
+			console.log(`Server is listening: http://localhost:${this.port}`)
+		})
+		this.setupLiveReload()
+	}
 }
 
-const serveStaticFiles = (req, res, next) => {
-    const urlNormalized = req.url === '/' ? '/index.html' : req.url
-    const ext = path.extname(urlNormalized)
-    const contentType = mimeTypes[ext] ?? 'text/plain'
-    const directory = ext != '.ico' ? 'src' : ''
-    const filePath = path.join(__dirname, directory, urlNormalized)
-
-    readFileWithLiveReload(filePath, contentType, (err, data) => {
-        if (err) {
-            return next(err)
-        }
-        res.writeHead(200, { "content-type": contentType })
-        res.end(data)
-    })
-}
-
-const handleError = (req, res) => {
-    const indexPath = path.join(__dirname, 'src', 'index.html')
-
-    readFileWithLiveReload(indexPath, 'text/html', (err, content) => {
-        if (err) {
-            res.writeHead(500, { "content-type": "text/plain" })
-            return res.end("Internal Server Error")
-        }
-        res.writeHead(301, { Location: '/', "content-type": 'text/html' })
-        res.end(content)
-    }) 
-}
-
-const server = http.createServer((req, res) => {
-    serveStaticFiles(req, res, (err) => {
-        if (err) {
-            handleError(req, res)
-        }
-    })
-})
-
-server.listen(3000, () => {
-    console.log(`Server is listening: http://localhost:${PORT}`)
-})
-
-if (process.env.NODE_ENV === 'development ') {
-    const liveReloadServer = livereload.createServer({
-        exts: ['html', 'js', 'css'],
-        debug: true
-    })
-
-    const watchDirectory = path.join(__dirname, 'src')
-
-    liveReloadServer.watch(watchDirectory)
-
-    let timer
-    fs.watch(watchDirectory, { recursive: true }, (eventType, fileName) => {
-        if (fileName) {
-            clearTimeout(timer)
-            timer = setTimeout(() => {
-                console.log(`File changed: ${fileName}`)
-                liveReloadServer.refresh('*')
-            }, 100)
-        }
-    })
-}
+const server = new StaticFileServer(PORT)
+server.start()
